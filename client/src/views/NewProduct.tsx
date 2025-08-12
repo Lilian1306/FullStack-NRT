2f0a5b77a98af9837d3afa742bb11abbbdc9864a
DaniM0613 <wwh.rj.92@gmail.com>
2025-08-12 16:21:50 -0600

    Rounting entre las diferentes paginas

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
 
 
 export default function NewProduct() {
   return (
-    <div>
-      NewProduct
-    </div>
+    <>
+      <div className="flex justify-between">
+         <h2 className="text-4xl font-black text-slate-500">Rigistrar Producto</h2>
+         <Link
+            to='/'
+            className="rounded-md bg-indigo-600 p-3 text-white text-sm font-bold hover:bg-indigo-500 shadow-sm"
+         >
+            Volver a productos
+         </Link>
+      </div>
+    </>
   )
 }
